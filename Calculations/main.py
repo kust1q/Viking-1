2a543f24d69493868bb02b516ccdb681ee1a59b9
gCTRL26 <strela48loko@gmail.com>
2024-12-13 23:17:08 +0300

    Adding angle graph

--- Calculations/main.py
+++ Calculations/main.py
@@ -1,14 +1,14 @@
-from math import pi
+from math import pi, atan
 from Angle_Functions import Linear, Parabolic, Elliptic
 import Create_Rocket
 import Main_Models
 import matplotlib.pyplot as plt
-
+from matplotlib.pyplot import figure
 
 thrust_vac = 4_900_000
 thrust_ground = 4_409_000
 mass_start = 198_472  # 78 472
-mass_end = 15_700
+mass_end = 39_200
 fuel_consumption = 200.99
 full_fuel = 16000
 resis_coefs = [
@@ -33,7 +33,7 @@ params = (
 
 
 def experimennts(function):
-    optimal_elliptic = [(0, 0), ([], []), ([], []), 10**10]
+    optimal_elliptic = [([], []), ([], []), 10**10]
     for lower in range(8000, 30001, 1500):
         for higher in range(30000, 60001, 1500):
             our_func = function((lower, 0), (higher, pi / 2))
@@ -45,19 +45,61 @@ def experimennts(function):
                 Viking_first.stages[0],
                 Viking_first.stages[0].max_time_moving,
             )
-            if delta_v < optimal_elliptic[3]:
-                optimal_elliptic[0] = (lower, higher)
-                optimal_elliptic[1] = traectory
-                optimal_elliptic[2] = orbit
-                optimal_elliptic[3] = delta_v
+            if delta_v < optimal_elliptic[2]:
+                optimal_elliptic[0] = traectory
+                optimal_elliptic[1] = orbit
+                optimal_elliptic[2] = delta_v
 
-    print("Необходимый delta_v", optimal_elliptic[3])
-    print(optimal_elliptic[0])
+    print("Необходимый delta_v", optimal_elliptic[2])
+    print("График изменения угла от координаты X")
+    X_angle_takeof = [0]
+    Y_angle_takeof = [0]
+    angles = []
+    for i in range(len(optimal_elliptic[0][0]) - 1):
+        if i == len(optimal_elliptic[0][0]) - 2:
+            X_angle_takeof.append(
+                optimal_elliptic[1][0][0] - optimal_elliptic[0][0][i + 1]
+            )
+        X_angle_takeof.append(optimal_elliptic[0][0][i + 1] - optimal_elliptic[0][0][i])
 
+    for i in range(len(optimal_elliptic[0][1]) - 1):
+        if i == len(optimal_elliptic[0][1]) - 2:
+            Y_angle_takeof.append(
+                optimal_elliptic[1][1][0] - optimal_elliptic[0][1][i + 1]
+            )
+        Y_angle_takeof.append(optimal_elliptic[0][1][i + 1] - optimal_elliptic[0][1][i])
+
+    for i in range(1, len(X_angle_takeof)):
+        if X_angle_takeof[i] == 0.0:
+            angles.append(pi / 2)
+            continue
+        angles.append(atan(Y_angle_takeof[i] / X_angle_takeof[i]))
+
+    X_angle_orbit = [optimal_elliptic[0][0][-1]]
+    Y_angle_orbit = [optimal_elliptic[1][0][-1]]
+    angles_orbit = [angles[-1]]
+    for i in range(len(optimal_elliptic[1][0]) - 1):
+        X_angle_orbit.append(optimal_elliptic[1][0][i + 1] - optimal_elliptic[1][0][i])
+
+    for i in range(len(optimal_elliptic[1][1]) - 1):
+        Y_angle_orbit.append(optimal_elliptic[1][1][i + 1] - optimal_elliptic[1][1][i])
+
+    for i in range(1, len(X_angle_orbit)):
+        if X_angle_orbit[i] == 0.0:
+            angles_orbit.append(pi / 2)
+            continue
+        angles_orbit.append(atan(Y_angle_orbit[i] / X_angle_orbit[i]))
+
+    plt.plot(optimal_elliptic[0][0], angles)
+    plt.plot(optimal_elliptic[1][0], angles_orbit)
+    plt.show()
+
+    print("График траектории ракеты")
+    figure(figsize=(10, 10))
     kerbin_orbit = Main_Models.DrawKerbinOrbit()
     plt.plot(*kerbin_orbit)
+    plt.plot(*optimal_elliptic[0])
     plt.plot(*optimal_elliptic[1])
-    plt.plot(*optimal_elliptic[2])
     plt.gca().set_aspect("equal", adjustable="box")
     plt.show()
 
